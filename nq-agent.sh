be2d2e938a6765b43519320ec3f4003749e54c17
William Gill <william.gill@anomalous.dev>
2022-02-04 19:47:04 -0600

    Update nq-agent.sh

--- nq-agent.sh
+++ nq-agent.sh
@@ -234,9 +234,9 @@ ping_as=$(prep $(num "$(ping -c 2 -w 2 208.67.222.222 | grep rtt | cut -d'/' -f4
 
 # Build data for post
 data_post=$( jq -n \
-                  --arg version "$(base "$version")" \
-                  --arg uptime "$(base "$uptime")" \
-                  --arg sessions "$(base "$sessions")" \
+                  --arg version "$version" \
+                  --arg uptime "$uptime" \
+                  --arg sessions "$sessions" \
                   '{version: $version, uptime: $uptime, sessions: $sessions}' )
 
 #data_post="token=${auth[0]}&data=$(base "$version") $(base "$uptime") $(base "$sessions") $(base "$processes") $(base "$processes_array") $(base "$file_handles") $(base "$file_handles_limit") $(base "$os_kernel") $(base "$os_name") $(base "$os_arch") $(base "$cpu_name") $(base "$cpu_cores") $(base "$cpu_freq") $(base "$ram_total") $(base "$ram_usage") $(base "$swap_total") $(base "$swap_usage") $(base "$disk_array") $(base "$disk_total") $(base "$disk_usage") $(base "$connections") $(base "$nic") $(base "$ipv4") $(base "$ipv6") $(base "$rx") $(base "$tx") $(base "$rx_gap") $(base "$tx_gap") $(base "$load") $(base "$load_cpu") $(base "$load_io") $(base "$ping_eu") $(base "$ping_us") $(base "$ping_as")"
